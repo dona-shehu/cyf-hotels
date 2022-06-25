bdc84effda719891431aff5d4ef6386d3d59d046
dona-shehu <shehu.donna@gmail.com>
2022-06-25 14:16:25 +0100

    adding bookings&products

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const pool = new Pool({
         rejectUnauthorized: false
     } 
 })
-  
+
+//run this comands in terminal
+  // export DATABASE_URL=postgres://codeyourfuture:donashehu@localhost:5432/cyf_hotels?sslmode=disable
+// PORT=9999 npm run start
+
 app.get("/hotels", function (req, res) {
     pool.query('SELECT * FROM hotels')
         .then((result) => res.json(result.rows))
@@ -20,6 +24,23 @@ app.get("/hotels", function (req, res) {
             res.status(500).json(error);
         });
 });
+app.get("/products", function (req, res) {
+    pool.query('SELECT * FROM products')
+        .then((result) => res.json(result.rows))
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json(error);
+        });
+});
+app.get("/bookings", function (req, res) {
+    pool.query('SELECT * FROM bookings')
+        .then((result) => res.json(result.rows))
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json(error);
+        });
+});
+
 
 app.listen(port, function () {
     console.log(`The server is listening on port ${port}`);
